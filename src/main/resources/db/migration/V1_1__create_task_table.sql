72a13d1f8ded57b49893639e3326dc836771e1ed
micha <michal.jeleniasty@gmail.com>
2023-09-19 22:48:38 +0200

    Minor container and table refactor

--- src/main/resources/db/migration/V1_1__create_task_table.sql
+++ src/main/resources/db/migration/V1_1__create_task_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS task (
-    id bigint PRIMARY KEY,
+    id bigint PRIMARY KEY AUTO_INCREMENT,
     title varchar(255) NOT NULL UNIQUE,
     description text,
     deadline timestamp,
