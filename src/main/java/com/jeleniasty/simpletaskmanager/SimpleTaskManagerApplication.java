bcd812a7f5dd17a8fa2f103bf4f7eca3009edf14
micha <michal.jeleniasty@gmail.com>
2023-09-19 20:32:59 +0200

    Initial commit

---
+++ src/main/java/com/jeleniasty/simpletaskmanager/SimpleTaskManagerApplication.java
@@ -0,0 +1,12 @@
+package com.jeleniasty.simpletaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTaskManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleTaskManagerApplication.class, args);
+  }
+}
